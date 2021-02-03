303e9568f9c30966e51c17e47a276152b7af2704
llaumegui <guillaumelef59@gmail.com>
2021-02-03 16:11:45 +0100

    FirstPush
    +AnimationManager
    +AI Slime

---
+++ AI/Assets/Resources/_Scripts/AI/AI.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AI : MonoBehaviour
+{
+	[Header("Wander")]
+	public Vector2 TimeWander;
+	public float DistanceWander;
+	public float WanderSpeed;
+
+	[Header("Aggro")]
+	public float AggroRange;
+	public float AggroSpeed;
+	GameObject _player;
+	bool _waitingAggro;
+	IEnumerator _aggroWait;
+
+	Vector3 _spawnPoint;
+	Rect _wanderArea;
+	Rect _aggroArea;
+
+	protected Vector2 _destination;
+	protected float _moveSpeed;
+
+	public enum States
+	{
+		wandering,
+		aggro,
+	}
+
+	States _currentState;
+
+
+	public virtual void Start()
+	{
+		_spawnPoint = transform.position;
+		_wanderArea = new Rect(new Vector2(_spawnPoint.x - DistanceWander / 2, _spawnPoint.y - DistanceWander / 2), Vector2.one * DistanceWander);
+		_player = GameObject.FindGameObjectWithTag("Player");
+		_aggroWait = WaitingAggro();
+
+		StartCoroutine(Wander());
+	}
+
+	public virtual void Update()
+	{
+		Move();
+		CheckAggro();
+	}
+
+	public virtual IEnumerator Wander()
+	{
+		if (_currentState != States.wandering)
+			SwitchState(States.wandering);
+
+		yield return new WaitForSeconds(Random.Range(TimeWander.x, TimeWander.y));
+
+		_destination = new Vector2(Random.Range(_wanderArea.xMin, _wanderArea.xMax), Random.Range(_wanderArea.yMin, _wanderArea.yMax));
+		_moveSpeed = WanderSpeed;
+
+		StartCoroutine(Wander());
+	}
+
+
+	public virtual void Move()
+	{
+		if (_destination.x < transform.position.x)
+			transform.localScale = new Vector3(-1, 1, 1);
+		else
+			transform.localScale = new Vector3(1, 1, 1);
+
+
+		transform.position = Vector3.MoveTowards(transform.position, _destination, Time.deltaTime *_moveSpeed);
+	}
+
+
+	public virtual void CheckAggro()
+	{
+		_aggroArea = new Rect(new Vector2(transform.position.x - AggroRange / 2, transform.position.y - AggroRange / 2), Vector2.one * AggroRange);
+		if (_aggroArea.Contains(_player.transform.position))
+		{
+			if(_currentState != States.aggro)
+			{
+				SwitchState(States.aggro);
+			}
+
+			_destination = _player.transform.position;
+			_moveSpeed = AggroSpeed;
+
+			_waitingAggro = false;
+			StopCoroutine(_aggroWait);
+			_aggroWait = WaitingAggro();
+		}
+		if(_currentState == States.aggro && !_waitingAggro)
+		{
+			_waitingAggro = true;
+			StartCoroutine(_aggroWait);
+		}
+	}
+
+	IEnumerator WaitingAggro()
+	{
+		yield return new WaitForSeconds(3);
+
+		SwitchState(States.wandering);
+	}
+
+	void SwitchState(States state)
+	{
+		StopAllCoroutines();
+
+		_currentState = state;
+		if (_currentState == States.wandering)
+			StartCoroutine(Wander());
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(_spawnPoint, Vector2.one * DistanceWander);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(transform.position, Vector2.one * AggroRange);
+	}
+}
---
+++ AI/Assets/Resources/_Scripts/AnimationManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+	public SpriteRenderer ShadowRenderer;
+	public float Framerate = .1f;
+	public AnimationsSprites[] AnimationList;
+
+	[System.Serializable]
+	public class AnimationsSprites
+	{
+		public AnimationManager.States State;
+		public List<Sprite> Sprites;
+		public float CustomFramerate;
+		public bool Loop;
+
+		[Space]
+		public bool DebugActivate;
+	}
+
+
+
+	int _index;
+	float _timer;
+	SpriteRenderer _renderer;
+
+	List<Sprite> _sprites;
+	bool _looping;
+	float _framerate;
+	bool _hasShadow;
+
+
+	public enum States
+	{
+		idle,
+		walk,
+		attack,
+		hurt,
+
+		debug,
+	}
+
+	void Start()
+	{
+		_renderer = GetComponent<SpriteRenderer>();
+		_sprites = SetSprites(States.idle);
+
+		if(ShadowRenderer != null)
+			_hasShadow = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		DisplaySprite();
+
+		CheckDebug();
+	}
+
+	void DisplaySprite()
+	{
+		_timer += Time.deltaTime;
+		if (_timer > _framerate)
+		{
+			_timer = 0;
+			_index = (_index + 1) % _sprites.Count;
+
+			if (!_looping && _index == 0)
+			{
+				_sprites = SetSprites(States.idle);
+			}
+
+			_renderer.sprite = _sprites[_index];
+			if (_hasShadow)
+				ShadowRenderer.sprite = _sprites[_index];
+		}
+	}
+
+	public void Play(States state)
+	{
+		_looping = false;
+		_sprites = SetSprites(state);
+	}
+
+	public void PlayLoop(States state)
+	{
+		Play(state);
+		_looping = true;
+	}
+
+	void CheckDebug()
+	{
+		States stateDebug = States.debug;
+		foreach (AnimationsSprites anim in AnimationList)
+		{
+			if (anim.DebugActivate && stateDebug == States.debug)
+				stateDebug = anim.State;
+
+			anim.DebugActivate = false;
+		}
+
+		if(stateDebug != States.debug)
+			_sprites = SetSprites(stateDebug);
+	}
+
+	List<Sprite> SetSprites(States state = States.debug, bool debugLoop = false)
+	{
+		AnimationsSprites animSelected = null;
+
+		foreach(AnimationsSprites anim in AnimationList)
+		{
+			if (anim.State == state)
+				animSelected = anim;
+		}
+
+		if(animSelected == null)
+			animSelected = AnimationList[0];
+
+		ResetAnim();
+
+		if(debugLoop)
+			_looping = animSelected.Loop;
+
+
+		if (animSelected.CustomFramerate > 0)
+			_framerate = animSelected.CustomFramerate;
+		else
+			_framerate = Framerate;
+
+		return animSelected.Sprites;
+	}
+
+	void ResetAnim()
+	{
+		_index = 0;
+		_timer = 0;
+	}
+}
